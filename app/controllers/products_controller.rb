37b9716b8656d93baf43fbd9d25b56e89993193c
Mika88 <mikaspivak@gmail.com>
2019-04-17 18:56:52 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,44 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def description
+    render plain: set_product.description
+  end
+
+  def inventory
+    if set_product.inventory
+      true.to_s
+    else
+      false.to_s
+    end
+  end
+
+  private
+
+  def set_product
+    Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
